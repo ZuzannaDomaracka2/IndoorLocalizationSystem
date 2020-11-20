7ef75782ac5e78d32a19452bfae2add7a3721345
RobertMadry <robert.l.madry@student.put.poznan.pl>
2020-11-20 18:39:12 +0100

    Dodano sprawdzenie aktywności WI-FI oraz Bluetooth.

--- app/src/main/java/com/pracowniatmib/indoorlocalizationsystem/MainActivity.java
+++ app/src/main/java/com/pracowniatmib/indoorlocalizationsystem/MainActivity.java
@@ -1,5 +1,9 @@
 package com.pracowniatmib.indoorlocalizationsystem;
 
+import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
@@ -47,16 +51,40 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         buttonEnableBt.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "ENABLE BLUETOOTH BUTTON CLICKED! ", Toast.LENGTH_SHORT).show();
+                BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+                if (mBluetoothAdapter == null) {
+                    Toast.makeText(MainActivity.this, "Device does not support BLUETOOTH! ", Toast.LENGTH_SHORT).show();
+                    buttonEnableBt.setText("Device does not support BLUETOOTH");
+                } else if (mBluetoothAdapter.isEnabled()) {
+                    Toast.makeText(MainActivity.this, "BLUETOOTH is ENABLE! ", Toast.LENGTH_SHORT).show();
+                    buttonEnableBt.setText("BLUETOOTH is ENABLE");
+                } else {
+                    Toast.makeText(MainActivity.this, "BLUETOOTH is DISABLE", Toast.LENGTH_SHORT).show();
+                    buttonEnableBt.setText("BLUETOOTH is DISABLE");
+                }
+
             }
         });
         buttonEnableWiFi.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "ENABLE WI-FI BUTTON CLICKED!", Toast.LENGTH_SHORT).show();
+                WifiManager wifi = (WifiManager)getSystemService(Context.WIFI_SERVICE);
+              if (wifi.isWifiEnabled())
+                {
+                    Toast.makeText(MainActivity.this, "WI-FI is ENABLE!", Toast.LENGTH_SHORT).show();
+                    buttonEnableWiFi.setText("WI-FI_is ENABLE");
+                }
+              else
+                  Toast.makeText(MainActivity.this, "WI-FI is DISABLE!", Toast.LENGTH_SHORT).show();
+                {
+                    buttonEnableWiFi.setText("WI-FI is DISABLE");
+                }
             }
         });
+
     }
 }

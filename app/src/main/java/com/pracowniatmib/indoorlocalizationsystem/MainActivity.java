12c3b807506e38b28a5550c8dbb292ac9f2c80ac
a <a>
2020-11-23 18:31:26 +0100

    firebase

--- app/src/main/java/com/pracowniatmib/indoorlocalizationsystem/MainActivity.java
+++ app/src/main/java/com/pracowniatmib/indoorlocalizationsystem/MainActivity.java
@@ -1,6 +1,5 @@
 package com.pracowniatmib.indoorlocalizationsystem;
 
-import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.net.wifi.WifiManager;
@@ -10,6 +9,8 @@ import android.view.View;
 import android.content.Intent;
 import android.widget.Button;
 import android.widget.Toast;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Objects;
 
@@ -19,6 +20,13 @@ public class MainActivity extends AppCompatActivity {
     Button buttonCheckPermissions;
     Button buttonEnableBt;
     WifiManager wifiManager;
+
+    DatabaseReference dataBuildings;
+    DatabaseReference dataUsers;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +40,19 @@ public class MainActivity extends AppCompatActivity {
         buttonEnableBt = findViewById(R.id.buttonEnableBtMenu);
         wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
 
+        dataBuildings= FirebaseDatabase.getInstance().getReference("Buildings");
+        dataUsers=FirebaseDatabase.getInstance().getReference("Users");
+        addBuildings();
+
+
+
+
+
         buttonStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(), MapActivity.class));
+
             }
         });
         buttonCheckDbConnection.setOnClickListener(new View.OnClickListener() {
@@ -67,5 +84,77 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        String userId = dataUsers.push().getKey();
+        dataUsers.child(userId).child("userId").setValue(userId);
+        dataUsers.child(userId).child("building_floor").setValue("1");
+        dataUsers.child(userId).child("building_id").setValue("123");
+        dataUsers.child(userId).child("coordinates").child("x").setValue("0");
+        dataUsers.child(userId).child("coordinates").child("y").setValue("0");
+
     }
+
+
+
+
+
+
+    public void addBuildings(){
+        //Building1
+        String id1=dataBuildings.push().getKey();
+        dataBuildings.child(id1).child("map_name").setValue("map_1");
+        dataBuildings.child(id1).child("name").setValue("building1");
+
+        // BLE Transmitter 1
+        dataBuildings.child(id1).child("floors").child("0").child("BLE Transmitters").child("BLE Transmitter 1").child("coordinates").child("x").setValue("1");
+        dataBuildings.child(id1).child("floors").child("0").child("BLE Transmitters").child("BLE Transmitter 1").child("coordinates").child("y").setValue("1");
+        dataBuildings.child(id1).child("floors").child("0").child("BLE Transmitters").child("BLE Transmitter 1").child("power").setValue("1");
+
+
+        //BLE Trasmitter 2
+        dataBuildings.child(id1).child("floors").child("0").child("BLE Transmitters").child("BLE Transmitter 2").child("coordinates").child("x").setValue("1");
+        dataBuildings.child(id1).child("floors").child("0").child("BLE Transmitters").child("BLE Transmitter 2").child("coordinates").child("y").setValue("1");
+        dataBuildings.child(id1).child("floors").child("0").child("BLE Transmitters").child("BLE Transmitter 2").child("power").setValue("1");
+
+        //WifiTrasmitter1
+        dataBuildings.child(id1).child("floors").child("0").child("WiFi Transmitters").child("WiFi Transmitter 1").child("coordinates").child("x").setValue("1");
+        dataBuildings.child(id1).child("floors").child("0").child("WiFi Transmitters").child("WiFi Transmitter 1").child("coordinates").child("y").setValue("1");
+        dataBuildings.child(id1).child("floors").child("0").child("WiFi Transmitters").child("WiFi Transmitter 1").child("power").setValue("1");
+
+        //Wifitransmitter2
+        dataBuildings.child(id1).child("floors").child("0").child("WiFi Transmitters").child("WiFi Transmitter 2").child("coordinates").child("x").setValue("1");
+        dataBuildings.child(id1).child("floors").child("0").child("WiFi Transmitters").child("WiFi Transmitter 2").child("coordinates").child("y").setValue("1");
+        dataBuildings.child(id1).child("floors").child("0").child("WiFi Transmitters").child("WiFi Transmitter 2").child("power").setValue("1");
+
+        //Building 2
+        String id2=dataBuildings.push().getKey();
+
+        dataBuildings.child(id2).child("map_name").setValue("map_2");
+        dataBuildings.child(id2).child("name").setValue("building2");
+
+        // BLE Transmitter 1
+        dataBuildings.child(id2).child("floors").child("0").child("BLE Transmitters").child("BLE Transmitter 1").child("coordinates").child("x").setValue("1");
+        dataBuildings.child(id2).child("floors").child("0").child("BLE Transmitters").child("BLE Transmitter 1").child("coordinates").child("y").setValue("1");
+        dataBuildings.child(id2).child("floors").child("0").child("BLE Transmitters").child("BLE Transmitter 1").child("power").setValue("1");
+
+        //BLE Trasmitter 2
+        dataBuildings.child(id2).child("floors").child("0").child("BLE Transmitters").child("BLE Transmitter 2").child("coordinates").child("x").setValue("1");
+        dataBuildings.child(id2).child("floors").child("0").child("BLE Transmitters").child("BLE Transmitter 2").child("coordinates").child("y").setValue("1");
+        dataBuildings.child(id2).child("floors").child("0").child("BLE Transmitters").child("BLE Transmitter 2").child("power").setValue("1");
+
+        //WifiTrasmitter1
+        dataBuildings.child(id2).child("floors").child("0").child("WiFi Transmitters").child("WiFi Transmitter 1").child("coordinates").child("x").setValue("1");
+        dataBuildings.child(id2).child("floors").child("0").child("WiFi Transmitters").child("WiFi Transmitter 1").child("coordinates").child("y").setValue("1");
+        dataBuildings.child(id2).child("floors").child("0").child("WiFi Transmitters").child("WiFi Transmitter 1").child("power").setValue("1");
+
+        //Wifitransmitter2
+        dataBuildings.child(id2).child("floors").child("0").child("WiFi Transmitters").child("WiFi Transmitter 2").child("coordinates").child("x").setValue("1");
+        dataBuildings.child(id2).child("floors").child("0").child("WiFi Transmitters").child("WiFi Transmitter 2").child("coordinates").child("y").setValue("1");
+        dataBuildings.child(id2).child("floors").child("0").child("WiFi Transmitters").child("WiFi Transmitter 2").child("power").setValue("1");
+
+
+    }
+
+
+
 }
